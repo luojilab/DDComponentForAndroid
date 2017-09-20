d1ad8aba54739d5b9ed304f17d9f9456a5087bee
leobert-lan <774057695@qq.com>
2017-09-20 20:04:57 +0800

    [anno] [change jsonComponent to jsonlib. Currently still use Router to fetch JsonService]

--- app/src/main/java/com/mrzhang/component/application/AppApplication.java
+++ app/src/main/java/com/mrzhang/component/application/AppApplication.java
@@ -2,6 +2,10 @@ package com.mrzhang.component.application;
 
 import android.app.Application;
 
+import com.ljsw.component.json.serviceimpl.JsonServiceImpl;
+import com.mrzhang.component.componentlib.router.Router;
+import com.mrzhang.componentservice.json.JsonService;
+
 /**
  * Created by mrzhang on 2017/6/15.
  */
@@ -16,6 +20,8 @@ public class AppApplication extends Application {
 //        Router.registerComponent("com.mrzhang.reader.applike.ReaderAppLike");
 //        Router.registerComponent("com.mrzhang.share.applike.ShareApplike");
 
+        // register jsonService
+        Router.getInstance().addService(JsonService.class.getSimpleName(), new JsonServiceImpl());
     }
 
 
