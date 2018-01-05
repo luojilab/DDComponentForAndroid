1fc1b51ee2e692798fa42e4bd7d6e7594c032114
mrzhang <zhangmingqing@luojilab.com>
2018-01-05 10:54:21 +0800

    add application in componentkotlin

---
+++ sharecomponentkotlin/src/main/runalone/java/com.luojilab.share.runalone/application/KotlinApplication.kt
@@ -0,0 +1,14 @@
+package com.luojilab.share.runalone.application
+
+import android.app.Application
+
+/**
+ * Created by mrzhang on 2018/1/5.
+ */
+class KotlinApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
