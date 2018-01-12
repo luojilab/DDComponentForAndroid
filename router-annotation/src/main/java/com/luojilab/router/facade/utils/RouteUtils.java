b5f1202659664f4b90608827bb97633d44984d30
mrzhang <zhangmingqing@luojilab.com>
2018-01-12 11:21:24 +0800

    fix  misspell

--- router-annotation/src/main/java/com/luojilab/router/facade/utils/RouteUtils.java
+++ router-annotation/src/main/java/com/luojilab/router/facade/utils/RouteUtils.java
@@ -12,7 +12,7 @@ public class RouteUtils {
 
     private static final String ROUTERTABLE = "RouterTable";
 
-    public static String fiestCharUpperCase(String str) {
+    public static String firstCharUpperCase(String str) {
         char[] ch = str.toCharArray();
         if (ch[0] >= 'a' && ch[0] <= 'z') {
             ch[0] = (char) (ch[0] - 32);
@@ -21,13 +21,13 @@ public class RouteUtils {
     }
 
     public static String genHostUIRouterClass(String host) {
-        String claName = ROUTERIMPL_OUTPUT_PKG + DOT + fiestCharUpperCase(host) + UIROUTER;
+        String claName = ROUTERIMPL_OUTPUT_PKG + DOT + firstCharUpperCase(host) + UIROUTER;
         return new String(claName);
     }
 
 
     public static String genRouterTable(String host) {
-        String claName = "./UIRouterTable/" + fiestCharUpperCase(host) + ROUTERTABLE + ".txt";
+        String claName = "./UIRouterTable/" + firstCharUpperCase(host) + ROUTERTABLE + ".txt";
         return new String(claName);
     }
 
