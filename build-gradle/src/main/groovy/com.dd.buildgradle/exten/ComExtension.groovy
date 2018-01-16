bec939f8572e00662b091779b86dc83c5ae91d5f
ouyangpeng <oypcz@foxmail.com>
2018-01-16 15:19:43 +0800

    1、删除gradle文件中部分冗余的语法 2、添加相应的注释便于理解代码

--- build-gradle/src/main/groovy/com.dd.buildgradle/exten/ComExtension.groovy
+++ build-gradle/src/main/groovy/com.dd.buildgradle/exten/ComExtension.groovy
@@ -1,12 +1,12 @@
 package com.dd.buildgradle.exten
 
-public class ComExtension {
+class ComExtension {
 
     /**
      * 是否自动注册组件，true则会使用字节码插入的方式自动注册代码
      * false的话，需要手动使用反射的方式来注册
      */
-    boolean isRegisterCompoAuto = false;
+    boolean isRegisterCompoAuto = false
 
     /**
      * 当前组件的applicationName，用于字节码插入。
